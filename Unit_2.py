085ef5be31bc33733adef262f63b061ffdd85da8
Bhanu-0711 <bhanu7pratap11@gmail.com>
2025-09-03 09:39:10 +0530

    Operators in python

---
+++ Unit_2.py
@@ -0,0 +1,20 @@
+#taking input in python
+'''
+a=int(input('Enter a no.'))
+if (a>10):
+    print('less than 10'):
+else:
+    print('more than 10')'''
+    
+#using shorthand if condition
+'''
+a=200
+b=300
+#1
+print('a is greater than b') if(a>b) else print('b is greater than a')
+#2
+if (a>b):print('A') '''
+
+'''
+print('A') if(a>b) else print('=') if a==b else print('B')'''
+
