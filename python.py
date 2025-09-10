f637ead286e2a4b4aff1bdcb0acc4db73c55bec5
Bhanu-0711 <bhanu7pratap11@gmail.com>
2025-09-10 11:37:22 +0530

    Basic Programs in Python

--- python.py
+++ python.py
@@ -91,16 +91,106 @@ print(tuple *2)'''
 '''for i in range(1,5,2):
     print(i)'''
     
-#Using match and pass keyword 
-'''
-var=x
-match x: #entering the varible name directly
-    case 1|2|3|4|5: 
-        pass  # helps in avoiding error in the code
-    case 2:
-        pass
-    case 3:
-        pass
-        
-'''
+#match statement
+'''day =10
+match day:
+    case 5:
+        print("Monday")
+    case 6:
+        print("Tuesday")
+    case 7:
+        print("Wednesday")
+    case 4:
+        print("Thursday")
+    case _:
+        print("Invalid day")'''
 
+#using multiple cases in match and also use conditions inside case
+'''day=5
+match day:
+    case 1|2|3|4|5 if day==5:
+        print("Weekday")
+    case _:
+        print("Invalid Day")'''
+        
+        
+#while loop
+'''i=1
+while i<6:
+    print(i)
+    i+=1'''
+
+#square of a no.    
+'''a=int(input('Enter a no.'))
+print(a**2)'''
+
+#sum of two nos.
+'''a=int(input('Enter no. 1'))
+b=int(input('Enter no. 2'))
+print(a+b)'''
+
+#swaping two variables 
+#1
+'''a=int(input('enter no. 1'))
+b=int(input('enter no. 2'))
+a=a+b
+b=a-b
+a=a-b
+print('Nos. are swapped')'''
+
+#2
+'''import math
+a=int(input('enter no. 1'))
+b=int(input('enter no. 2'))
+swap(a,b)'''
+
+##Guessing Number Game 
+'''import math
+import random
+x=int(input('Enter the range:- '))
+y=str(x//2)
+print('You will get only '+y+' chances') 
+a=random.randint(1,x)
+for i in range(1,(x//2)+1):
+    b=int(input('Enter the no. you guess:- '))
+    if (a==b):
+        print('You guessed it right')
+        print(f'The no. was {a}')
+        flag=1
+        break
+    else:
+        print('You guessed wrong')
+        flag=0
+        pass
+if(flag==1):
+    pass
+else:
+    print(f'The no. was {a}')'''
+
+#check no is even or odd
+'''a=int(input('enter a no'))
+if(a%2==0):
+    print(f'{a} is Even')
+else:
+    print(f'{a} is Odd')'''
+    
+#converting celcius to fahrenheit
+'''a=int(input('enter temp in celcius'))
+b=int(input('enter temp in fahrenheit'))
+print('temp in fahrenheit is',(9/5*a)+32)
+print('temp in celcius is',(b-32)*(5/9))'''
+
+#type of a variable
+'''a=int(input('enter no'))
+b=bool(input())
+print(type(a),type(b))'''
+
+#simple interest
+'''p=int(input('enter principle'))
+r=float(input('enter rate of interest'))
+t=int(input('enter time'))
+print('The simple interest is ',(p*r*t)/100)'''
+
+#reverse of a string
+'''a=input('enter a string:-  ')
+print(a[::-1])'''
